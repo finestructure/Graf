788fe523746a0060f74ffbb843496d2b8d64eee6
Sven A. Schmidt <sas@abstracture.de>
2012-01-02 15:47:53 +0100

    Initial Commit

---
+++ Sinma/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 02.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
