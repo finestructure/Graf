07e5ac39509fcdf41468f9928fd0dd26c26b59f4
Sven A. Schmidt <sas@abstracture.de>
2012-01-04 12:34:44 +0100

    Added SettingsViewController

---
+++ Sinma/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 04.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
