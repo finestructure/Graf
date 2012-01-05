bc8252ec694c799bce61e0a7b8a47217273c95ea
Sven A. Schmidt <sas@abstracture.de>
2012-01-05 14:29:14 +0100

    Added VideoViewController

---
+++ Sinma/VideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoViewController.h
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 05.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoViewController : UIViewController
+
+@end
