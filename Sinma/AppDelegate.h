0803e0c2753e85e64c82adcc3d637da14f6ef02c
Sven A. Schmidt <sas@abstracture.de>
2012-01-10 15:25:40 +0100

    Changed root view controller to video controller

--- Sinma/AppDelegate.h
+++ Sinma/AppDelegate.h
@@ -8,12 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@class ViewController;
+@class VideoViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) VideoViewController *viewController;
 
 @end
