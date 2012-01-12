7bebf11a17f55f18edca970d4881f3c28024b929
Sven A. Schmidt <sas@abstracture.de>
2012-01-12 17:12:14 +0100

    Attempt at submitting image
    Problems: didn't honor the timeout value (ran 240s instead of 60), didn't return result.

--- Sinma/ImageProcessor.m
+++ Sinma/ImageProcessor.m
@@ -46,8 +46,32 @@ const char *kPass = "i8Kn37rD8v";
 
 - (NSString *)processImage:(UIImage *)image
 {
-  NSLog(@"balance: %f", [self balance]);
-  return @"";
+  NSString *result = nil;
+  NSData *data = UIImagePNGRepresentation(image);
+  
+  dbc_captcha *captcha = (dbc_captcha *)malloc(sizeof(dbc_captcha));
+  if (dbc_init_captcha(captcha)) {
+    NSLog(@"Failed initializing a CAPTCHA instance");
+  } else {
+    
+    if (dbc_decode(_client, captcha, [data bytes], [data length], 30)) {
+      if (_client->balance <= 0) {
+        NSLog(@"Insufficied funds\n");
+      } else {
+        NSLog(@"Failed uploading/solving image\n");
+      }
+    } else if (!captcha->is_correct) {
+      NSLog(@"CAPTCHA was marked as invalid by an operator, check if it is in fact a CAPTCHA image and not corrupted");
+    } else {
+      NSLog(@"%d %s\n", captcha->id, captcha->text);
+      result = [NSString stringWithCString:captcha->text encoding:NSASCIIStringEncoding];
+    }
+
+    dbc_close_captcha(captcha);
+  }
+  free(captcha);
+  
+  return result;
 }
 
 
