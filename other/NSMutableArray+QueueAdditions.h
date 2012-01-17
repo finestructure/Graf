6faddc8db644c4524166e41f235f17de870332e1
Sven A. Schmidt <sas@abstracture.de>
2012-01-17 10:01:15 +0100

    Added queue additions

---
+++ other/NSMutableArray+QueueAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+QueueAdditions.h
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 17.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray_QueueAdditions : NSObject
+
+@end
---
+++ other/NSMutableArray+QueueAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+QueueAdditions.m
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 17.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "NSMutableArray+QueueAdditions.h"
+
+@implementation NSMutableArray_QueueAdditions
+
+@end
