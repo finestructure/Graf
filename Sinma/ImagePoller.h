4f047c866c1459b09247b25295e3712a5b97382c
Sven A. Schmidt <sas@abstracture.de>
2012-01-23 17:13:51 +0100

    Added ImagePoller

---
+++ Sinma/ImagePoller.h
@@ -0,0 +1,13 @@
+//
+//  ImagePoller.h
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 23.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImagePoller : NSObject
+
+@end
---
+++ Sinma/ImagePoller.m
@@ -0,0 +1,13 @@
+//
+//  ImagePoller.m
+//  Sinma
+//
+//  Created by Sven A. Schmidt on 23.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "ImagePoller.h"
+
+@implementation ImagePoller
+
+@end
