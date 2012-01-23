2c6232ecdd2b06e604e57cef6f62dfe900b1cb21
Sven A. Schmidt <sas@abstracture.de>
2012-01-23 17:18:40 +0100

    Added test skeleton

---
+++ Tests/ImagePollerTest.m
@@ -0,0 +1,32 @@
+#import <GHUnitIOS/GHUnit.h> 
+#import "ImagePoller.h"
+
+@interface ImagePollerTest : GHAsyncTestCase { }
+
+@end
+
+
+@implementation ImagePollerTest
+
+
+
+
+- (void)setUp {
+  [super setUp];  
+}
+
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+
+#pragma mark - tests
+
+
+- (void)test_01_poller {
+  GHAssertTrue(YES, nil);
+}
+
+
+@end
