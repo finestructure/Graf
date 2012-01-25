33422d65688a32b0b0183bc09230c8e8b30125cd
Sven A. Schmidt <sas@abstracture.de>
2012-01-25 14:15:52 +0100

    Added Image

---
+++ Main/Image.h
@@ -0,0 +1,14 @@
+//
+//  Image.h
+//  Graf
+//
+//  Created by Sven A. Schmidt on 25.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Image : NSObject
+
+
+@end
---
+++ Main/Image.m
@@ -0,0 +1,13 @@
+//
+//  Image.m
+//  Graf
+//
+//  Created by Sven A. Schmidt on 25.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "Image.h"
+
+@implementation Image
+
+@end
