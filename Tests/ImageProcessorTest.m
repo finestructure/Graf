3b01166f4e01b50cc0339e6713c42b84ac38779a
Sven A. Schmidt <sas@abstracture.de>
2012-01-27 10:25:31 +0100

    ImageProcessor test

---
+++ Tests/ImageProcessorTest.m
@@ -0,0 +1,30 @@
+#import <GHUnitIOS/GHUnit.h> 
+#import "ImageProcessor.h"
+
+
+@interface ImageProcessorTest : GHAsyncTestCase { }
+
+@end
+
+
+@implementation ImageProcessorTest
+
+
+- (void)setUp {
+  [super setUp];  
+}
+
+
+- (void)tearDown {
+  [super tearDown];
+}
+
+
+#pragma mark - tests
+
+
+- (void)test_01 {
+}
+
+
+@end
