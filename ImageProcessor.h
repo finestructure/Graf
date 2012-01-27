fd6cd691562f2e963c9365cb072bdf9f6c4c6647
Sven A. Schmidt <sas@abstracture.de>
2012-01-27 10:20:38 +0100

    Added ImageProcessor

---
+++ ImageProcessor.h
@@ -0,0 +1,13 @@
+//
+//  ImageProcessor.h
+//  Graf
+//
+//  Created by Sven A. Schmidt on 27.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageProcessor : UITableViewCell
+
+@end
---
+++ ImageProcessor.m
@@ -0,0 +1,29 @@
+//
+//  ImageProcessor.m
+//  Graf
+//
+//  Created by Sven A. Schmidt on 27.01.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "ImageProcessor.h"
+
+@implementation ImageProcessor
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
